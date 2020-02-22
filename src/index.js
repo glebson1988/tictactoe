fdc6e0dc5925b14532dcc0932ae25521583c9c8a
Gleb Grishakov <glebson1988@yandex.ru>
2020-02-22 12:23:27 +0300

    add handleClick && change Square to functional component

--- src/index.js
+++ src/index.js
@@ -2,32 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
+function Square(props) {
     return (
       <button
-        className="square"
-        onClick={() => this.setState({value: 'X'})}
-      >
-        {this.state.value}
+        className="square" onClick={props.onClick}>
+        {this.props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null),
+      squares: Array(9).fill(null)
     };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 
   renderSquare(i) {
